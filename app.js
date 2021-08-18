d7e563109f4f1992de9f111e443d1c9561004850
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:12:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+                    // QUESTION#01
+// var array=[[],[],[]]
+// document.write(array)
+                    // QUESTION#02
+// var array=[[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// document.write(array[0][0],"",array[0][1],"",array[0][2],"",array[0][3],"<br/>",
+//array[1][0],"",array[1][1],"",array[1][2],"",array[1][3],"<br/>",
+//array[2][0],"",array[2][1],"",array[2][2],"",array[2][3],"<br/>")
+                    // QUESTION#03
+// for(var i=1;i<=10;i++){
+//     document.write(i,"<br/>")}
+                    // QUESTION#04
+// var tableNumber=+prompt("Enetr a table Number")   
+// var tableLength=+prompt("Enter a table length")
+// for(var i=1;i<=tableLength;i++)                 
+// {document.write(tableNumber," X ",i," = ",tableNumber*i,"<br/>")}
+                    // QUESTION#05
+// var array=["apple","banana","mango","orange","strawberry"]
+// for(var i=0;i<=array.length;i++){
+// document.write(array[i],"<br/>","<br/>")}
+// for(var i=0;i<=array.length;i++){
+//     document.write("Element at Index ",i," is ",array[i],"<br/>")}
+                    // QUESTION#06
+// document.write("<b>Counting:</b></br>")
+// for(var i=1;i<=15;i++){
+// document.write(i,", ")}
+// document.write("</br><b>Reverse:</b></br>")
+// for(var i=15;i>=1;i--){
+// document.write(i,", ")}
+// document.write("</br><b>Even:</b></br>")
+// for(var i=0;i<=20;i++){
+//     if(i%2==0){
+// document.write(i,", ")}}
+// document.write("</br><b>Odd:</b></br>")
+// for(var i=1;i<=20;i++){
+//     if(i%2!==0){
+// document.write(i,", ")}}
+// document.write("</br><b>Series:</b></br>")
+// for(var i=1;i<=20;i++){
+//     if(i%2==0){
+// document.write(i,"k, ")}}
+                     // QUESTION#07
+// var a=["cake","apple pie","cie","chips","patties"]
+// var cookie=prompt("What do yo want to eat?\ncake applepie cie ahips patties")
+// for(var i=0;i<=a.length;i++)
+// if(cookie==a[i]){
+// document.write("Your required cookie: ",cookie,"is at index of ",i)
+// break}
+// if(cookie!==a[i]){
+// alert("Apology Sir! Your Desired Cookie Isn't Available")}
+//                      QUESTION#08
+// var a=[3 , 6, 2, 56, 32, 5, 89, 32]
+// var c=a.length
+// for(var i=0;i<a.length;i++){
+//     if(a[i]>c){
+//     c=a[i]
+//     }
+// }document.write("Array Items: ",a,"<br/>","Largest Number is: ",c)
+                    //  QUESTION#09
+// var a=[3 , 6, 2, 56, 32, 5, 89, 32]
+// var c=a.length
+// for(var i=0;i<a.length;i++){
+//     if(a[i]<c){
+//     c=a[i]
+//     }
+// }document.write("Array Items: ",a,"<br/>","Smallest Number is: ",c)
+                    //  QUESTION#09
+// var num=5
+// for(var i=1; i<=20;i++){
+//     c=num*i
+//     document.write(c,",")
+// }
